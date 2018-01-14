7cac2d13da24830693a9a78a5a0655c51cd328cd
Ross <ross@tests-MacBook-Pro.local>
2018-01-14 16:16:16 -0600

    initialize everything, set up stage

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('server/public'));
+
+
+app.listen(port, function(){
+    console.log('listening on port:', port);
+})
