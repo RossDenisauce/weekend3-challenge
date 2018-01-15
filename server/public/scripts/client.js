fb68dcebc737e67d121c92da9cc6a99023db3b9a
Ross <ross@tests-MacBook-Pro.local>
2018-01-14 18:26:09 -0600

    get almost working, post close as well

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,5 +2,48 @@ $(document).ready(onStart);
 
 function onStart(){
     console.log('JQ');
-    
+    getTask();
+    // $('#submitTask').on('click', addTask);
+}
+
+// function addTask(){
+//     let newTask = {
+//         author: $('#authorInput').val(),
+//         description: $('#taskInput').val()
+//     };
+
+//     $.ajax({
+//         method: 'POST',
+//         url: '/tasks',
+//         data: newTask,
+//         success: function(response){
+//             console.log('POST success', response);
+            
+//         }
+//     });
+// }
+
+function getTask(){
+    $.ajax({
+        method: 'GET',
+        url: '/tasks',
+        success: function(response){
+            console.log('GET success', response);
+            appendTask(response);
+        }
+    });
+}
+
+function appendTask(task){
+    $('#taskSpace').append(`
+        <li class="task">
+        <p>Who's Task: ${task.author}, Task: ${task.description}, Completed? ${task.completion}</p>
+        </li>
+        <select id="completionDropdown">
+            <option value="Yes">Yes</option>
+            <option value="No">No</option>
+        </select>
+        <button id="submitComplete">Completion</button>
+        <button id="deleteButon">Delete</button>
+    `);
 }
--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -2,4 +2,23 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+router.get('/', (req, res) => {
+    queryText = `SELECT * FROM tasks`;
+    pool.query(queryText)
+    .then((result) => {
+        console.log('query results: ', result);            
+        res.send(result.rows);
+    })
+    .catch((err) => {
+        console.log('error making select query:', err);
+        res.sendStatus(500);
+    });
+});
+
+// router.post('/', (req, res) => {
+//     console.log('req.body:', req.body);
+//     queryText = `INSERT INTO tasks (author, description) VALUES ($1, $2)`
+    
+// });
+
 module.exports = router;
